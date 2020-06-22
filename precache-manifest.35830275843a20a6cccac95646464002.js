99ff36c81c78c36d8ea87be3e1b22f2e52a4b825
tquangdo <trangia61@gmail.com>
2020-06-22 14:47:07 +0900

    Updates

---
+++ precache-manifest.35830275843a20a6cccac95646464002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1453125d681f36fad53d98c2249e3f",
+    "url": "/ReactJSGioHangOnlineRedux/index.html"
+  },
+  {
+    "revision": "2b2e89fcff4132dca83a",
+    "url": "/ReactJSGioHangOnlineRedux/static/js/2.6a381a7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJSGioHangOnlineRedux/static/js/2.6a381a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e3bae816699c45e044",
+    "url": "/ReactJSGioHangOnlineRedux/static/js/main.7354cf23.chunk.js"
+  },
+  {
+    "revision": "a5f6cd9c0f0831fb8a32",
+    "url": "/ReactJSGioHangOnlineRedux/static/js/runtime-main.eb562922.js"
+  }
+]);
